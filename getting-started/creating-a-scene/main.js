13d148618293a7a35f903a8ed7643cdecbf549a4
Vicky Adi Firmansyah <vickyadi243@gmail.com>
2025-08-09 20:44:11 +0700

    feat: add light

--- getting-started/creating-a-scene/main.js
+++ getting-started/creating-a-scene/main.js
@@ -14,11 +14,20 @@ renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00,
-});
+// const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+// Add light
+// To apply cube affected with light use MeshPhongMaterial not MeshBasicMaterial
+{
+  const color = 0xffffff;
+  const intensity = 5;
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(0, 5, 10);
+  scene.add(light);
+}
 
 camera.position.z = 3;
 
